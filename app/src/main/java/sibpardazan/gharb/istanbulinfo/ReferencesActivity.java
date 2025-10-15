a7fb45c4cd1a3b4cbee101eec07b250288e2eefe
Mohsen Shamohammadi <mohsen.shamohammadi@atolls.com>
2025-10-15 23:47:58 +0200

    update images

--- app/src/main/java/sibpardazan/gharb/istanbulinfo/ReferencesActivity.java
+++ app/src/main/java/sibpardazan/gharb/istanbulinfo/ReferencesActivity.java
@@ -44,7 +44,26 @@ public class ReferencesActivity extends AppCompatActivity {
     private void setupReferencesList() {
         // Define the reference URLs
         String[] referencesArray = {
-            "https:/test.com"
+            "https://www.lonelyplanet.com/news/hagia-sophia-istanbul-mosque",
+            "https://www.bluemosquetickets.com/about-blue-mosque/",
+            "https://www.topkapipalace-tickets.com/topkapi-palace-entrance/",
+            "https://en.wikipedia.org/wiki/S%C3%BCleymaniye_Mosque",
+            "https://www.througheternity.com/en/blog/things-to-do/The-Grand-Bazaar-in-Istanbul-A-Shoppers-Paradise.html",
+            "https://istanbul-tourist-information.com/dolmabahce-palace-visitor-uptade/",
+            "https://www.galata-tower.com/de",
+            "https://www.barcelo.com/guia-turismo/en/turkey/istanbul/things-to-do/galata-bridge/",
+            "https://www.lonelyplanet.com/turkey/istanbul/sultanahmet/attractions/basilica-cistern/a/poi-sig/402088/1324696",
+            "https://www.viator.com/de-AT/tours/Istanbul/Small-Group-Istanbul-Walking-Tour-Hagia-Sophia-Museum-and-the-Blue-Mosque/d585-5744ISTWALKING",
+            "https://www.gothrace.com/site/news/maidens-tower---istanbul/169",
+            "https://www.guidedistanbultours.com/en/blog/beylerbeyi-palace",
+            "https://www.gettyimages.de/fotos/istiklal-avenue",
+            "https://www.tripadvisor.de/Attraction_Review-g293974-d6223392-Reviews-Ahrida_Synagogue-Istanbul.html",
+            "https://visitbursa.org/mekanlar/iznik-nilufer-hatun-imareti-turk-islam-eserleri-muzesi-252",
+            "https://en.wikipedia.org/wiki/Atat%C3%BCrk_Airport",
+            "https://theistanbulinsider.com/the-egyptian-or-spice-bazaar-the-hottest-spot-in-town/",
+            "https://www.tripadvisor.com/Attraction_Review-g293974-d295153-Reviews-Rumeli_Fortress-Istanbul.html",
+            "https://en.wikipedia.org/wiki/G%C3%BClhane_Park",
+            "https://www.thecollector.com/what-are-the-theodosian-walls-of-constantinople/"
         };
 
         referencesList = new ArrayList<>(Arrays.asList(referencesArray));
